f80c88321a01089968015195040cf4edd65c8b9c
mdsaad2305 <mdsaad2303@gmail.com>
2025-01-01 05:21:43 +0530

    Used mockito and junit5 for basic testing of service layer of user-service

--- user-service/src/main/java/com/microservices/adoptify/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/microservices/adoptify/user_service/UserServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-// @EnableFeignClients
 public class UserServiceApplication {
 
   public static void main(String[] args) {
